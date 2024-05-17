68029fdab92798ae7ed510028da1d2559109e2ee
rbd3 <rabedesanaandry01@gmail.com>
2024-05-17 06:54:29 -0600

    Add whoImI algo

---
+++ whoImI.js
@@ -0,0 +1,11 @@
+function WhoImI() {
+    /* The Code Riddle: I'm thinking of a number. 
+    When you triple me and subtract 4, you get 20. What's my value? */
+
+    let x;
+     x = (20 + 4) / 3;
+    return x;
+}
+
+let ou = WhoImI()
+console.log(ou)
